c52b25d58ea6297fb770a5543df115543fd59ced
陈 <1105882406@qq.com>
2017-05-22 11:04:43 +0800

    666

---
+++ Think/ThinkPHP/Library/Org/Util/Cart.class.php
@@ -0,0 +1,151 @@
+<?php
+namespace Org\Util;
+class Cart{
+
+    public function __construct() {
+        if(!isset($_SESSION['cart'])){
+            $_SESSION['cart'] = array();
+        }
+    }
+ 
+    /*
+    添加商品
+    param int $id 商品主键
+          string $name 商品名称
+          float $price 商品价格
+          int $num 购物数量
+    */
+    public  function addItem($id,$name,$goodsArr) {
+        //如果该商品已存在则直接加其数量
+        if (!isset($_SESSION['cart'][$id])) {
+                     $_SESSION['cart'][$id]=array(
+                            'name'=>$name
+                    );
+        }
+
+        if(isset($_SESSION['cart'][$id][$attr_id])){
+               $this->incNum($id,$attr_id,$num);
+                return;
+        }
+        
+        $_SESSION['cart'][$id][$goodsArr['attr_id']] = $goodsArr;
+        
+     
+    }
+    
+
+
+    /*
+    修改购物车中的商品数量
+    int $id 商品主键
+    int $num 某商品修改后的数量，即直接把某商品
+    的数量改为$num
+    */
+    public function modNum($id,$num=1) {
+        if (!isset($_SESSION['cart'][$id])) {
+            return false;
+        }
+        $_SESSION['cart'][$id]['num'] = $num;
+    }
+ 
+    /*
+    商品数量+1
+    */
+    public function incNum($id,$attr_id,$num=1) {
+        if (isset($_SESSION['cart'][$id])) {
+            $_SESSION['cart'][$id][$attr_id]['num'] += $num;
+        }
+    }
+ 
+    /*
+    商品数量-1
+    */
+    public function decNum($id,$num=1) {
+        if (isset($_SESSION['cart'][$id])) {
+            $_SESSION['cart'][$id][$attr_id]['num'] -= $num;
+        }
+ 
+        //如果减少后，数量为0，则把这个商品删掉
+        if ($_SESSION['cart'][$id][$attr_id]['num'] <1) {
+            $this->delItem($id,$attr_id);
+        }
+    }
+ 
+    /*
+    删除商品
+    */
+    public function delItem($id,$attr_id) {
+          if(count( $_SESSION['cart'][$id][$attr_id] )<=0 )
+                 unset($_SESSION['cart'][$id]);
+          else
+                unset($_SESSION['cart'][$id][$attr_id]);
+    }
+     
+    /*
+    获取单个商品
+    */
+    public function getItem($id) {
+        return $_SESSION['cart'][$id];
+    }
+ 
+    /*
+    查询购物车中商品的种类
+    */
+    public function getCnt() {
+        return count($_SESSION['cart']);
+    }
+     
+    /*
+    查询购物车中某商品的属性
+    */
+    public function getAttr($id){
+       $data = $_SESSION['cart'][$id];
+       unset($data['name']);
+       count($data);
+    }
+
+    /*
+    查询购物车中商品的个数
+    */
+    public function getNum(){
+        if ($this->getCnt() == 0) {
+            //种数为0，个数也为0
+            return 0;
+        }
+ 
+        $sum = 0;
+        $data = $_SESSION['cart'];
+        foreach ($data as $item) {
+            foreach ($item as $key => $value) {
+                if($key!='id')
+                      $sum += $value['num'];
+            }
+          
+        }
+        return $sum;
+    }
+    
+
+    /*
+    购物车中商品的总金额
+    */
+    public function getPrice() {
+        //数量为0，价钱为0
+        if ($this->getCnt() == 0) {
+            return 0;
+        }
+        $price = 0.00;
+        $data = $_SESSION['cart'];
+        foreach ($data as $item) {
+            $price += $item['num'] * $item['price'];
+        }
+        return sprintf("%01.2f", $price);
+    }
+ 
+    /*
+    清空购物车
+    */
+    public function clear() {
+        $_SESSION['cart'] = array();
+    }
+}
---
+++ eshop/Home/Controller/CommonController.class.php
@@ -0,0 +1,11 @@
+<?php
+	namespace Home\Controller;
+	use Think\Controller;
+	class CommonController extends Controller {
+
+		public function __construct(){
+			parent::__construct();
+			session_start;
+		}
+
+	}
