18a82f38082d21416ea57a3a5d49f03340022de2
peitds17 <necomori246@gmail.com>
2017-05-22 20:47:25 +0800

    后台用户

---
+++ eshop/Admin/Controller/UserController.class.php
@@ -0,0 +1,86 @@
+<?php
+	namespace Admin\Controller;
+
+	class UserController extends EqualController{
+		/**
+		*['创建User类实例']
+		*/
+		public function __construct(){
+			parent::__construct();
+			$this->model = D('User');
+		}
+
+		/**
+		*[ajax无刷新修改is_use]
+		*/
+		public function ajaxIsUse(){
+			$id = I('get.id','');
+			$userOne = $this->model->find($id);
+			$userOne['is_use'] = $userOne['is_use']?0:1;
+			if($this->model->save($userOne))
+				echo $userOne['is_use'];
+		}
+
+		/**
+		*['user数据显示']
+		*/
+		public function show(){
+			$id = I('get.id','');
+			$data = array();
+			$data['userOne'] = $this->model->getUserOne($id);
+			$data['userOne'] = $data['userOne'][0];
+			$this->assign($data);
+			$this->assignHead('会员详细信息',U('list'),'会员详细信息');
+			$this->display();
+		}
+
+		/**
+		*[user数据表显示和搜索]
+		*/
+		public function list(){
+			$font = 1;
+			$data = array();
+			$data = $this->model->search($font);
+			$this->assign($data);
+			//$this->assignHead('黑名单列表',U('add'),'黑名单列表');
+			$this->display();
+		}
+
+		/**
+		*[user数据表黑名单显示和搜索]
+		*/
+		public function black(){
+			$font = 0;
+			$data = array();
+			$data = $this->model->search();
+			$this->assign($data);
+			//$this->assignHead('黑名单列表',U('add'),'黑名单列表');
+			$this->display();
+		}
+
+		/**
+		*[user数据拉进黑名单]
+		*/
+		public function inblack(){
+			$where['id'] = I('get.id','');
+			$data['is_use'] = 2;
+			$p = I('get.p',0);
+
+			$this->model->where($where)->save($data);
+
+			$this->success( '拉进黑名单成功',U('list',array('p'=>$p)) );
+		}
+
+		/**
+		*[user数据拉离黑名单]
+		*/
+		public function outblack(){
+			$where['id'] = I('get.id','');
+			$data['is_use'] = 1;
+			$p = I('get.p',0);
+
+			$this->model->where($where)->save($data);
+
+			$this->success( '离开黑名单成功',U('black',array('p'=>$p)) );
+		}
+	}
---
+++ eshop/Admin/Model/UserModel.class.php
@@ -0,0 +1,74 @@
+<?php
+	namespace Admin\Model;
+	use Think\Model;
+	class UserModel extends Model{
+		/**
+		*[搜索+分页]
+		*@return array 		$data[搜索后+分页的数据]
+		*/
+		public function search($font){
+
+			/**********************搜索**********************/
+			$where = array();
+			if( ( $search_key = I('get.search_key','') ) && ( $search_val = I('get.search_val','') ) ){
+				if($search_key == 'account'){
+					$where['account'] = array('like','%'.$search_val.'%');
+				}
+				if($search_key == 'username'){
+					$where['username'] = array('like','%'.$search_val.'%');
+				}
+				if($search_key == 'tel'){
+					$where['tel'] = array('like','%'.$search_val.'%');
+				}
+				if($search_key == 'email'){
+					$where['email'] = array('like','%'.$search_val.'%');
+				}
+				elseif($search_key == 'id'){
+					$where[$search_key] = array('eq',$search_val);
+				}
+			}
+
+			if( ($is_use = I('get.is_use',false)) === false ){
+				if($font == 1){
+					$where['is_use'] = array('between','0,1');
+				}elseif($font == 0){
+					$where['is_use'] = array('eq','2');
+				}
+			}
+
+			//按is_use是否显示搜索
+			if( ($is_use = I('get.is_use',false)) !== false ){
+				$where['is_use'] = array('eq',$is_use);
+			}
+
+			/**********************分页**********************/
+			$data = array();
+			$count = $this->where($where)->count();
+			$data['count'] = $count;
+			$page = new \Think\Page($count,C('YeShu'));
+			$data['show'] = $page->show();
+
+			$userList = $this->field('id,sm_icon,account,username,tel,email,regtime,is_use')->where($where)->limit($page->firstRow.','.$page->listRows)->select();
+
+			foreach ($userList as $key => $value) {
+				$is_use = '0';
+				if($value['is_use']){
+					$is_use = '1';
+				}
+				$userList[$key]['is_use'] = $is_use;
+			}
+			$data['userList'] = $userList;
+			return $data;
+		}
+
+		/**
+		*[修改显示]
+		*@param int 		$id[要显示的数据id]
+		*@return array 		$userOne[修改后的数据]
+		*/
+		public function getUserOne($id){
+			$data['id'] = $id;
+			$userOne = $this->field('icon,account,username,tel,email,regtime,credit,exp,level,birthdate,realname')->where($data)->select();
+			return $userOne;
+		}
+	}
