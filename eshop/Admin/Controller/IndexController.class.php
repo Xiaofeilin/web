c5ec0b5c0e2c79ce81058df299cd9cd71db72b49
Xiaofeilin <363209741@qq.com>
2017-05-23 15:46:11 +0800

    欢迎信息

--- eshop/Admin/Controller/IndexController.class.php
+++ eshop/Admin/Controller/IndexController.class.php
@@ -3,34 +3,6 @@
 
 	class IndexController extends EqualController{
 
-		// main系统信息
-		public function main(){
-
-			$model = M();
-			$v = $model->query("select VERSION() as ver");
-
-			$main['php_os'] = PHP_OS;
-			$main['SERVER_SOFTWARE'] = $_SERVER['SERVER_SOFTWARE'];
-
-			$main['phpver'] = PHP_VERSION;
-			$main['mysqlver'] = $v[0]['ver'];
-			
-			$main['sockets'] = extension_loaded('sockets')?'是':'否';
-			$main['timezone'] = date_default_timezone_get();
-
-			$jpeg = gd_info()['JPEG Support']?'JPEG':'';
-			$png = gd_info()['PNG Support']?'PNG':'';
-			$gif = gd_info()['GIF Read Support'] && gd_info()['GIF Create Support']?'GIF':'';
-
-			$main['gdver'] = gd_info()['GD Version'].'('.$jpeg.' '.$gif.' '.$png.')';
-			$main['zlib'] = extension_loaded('zlib')?'是':'否';
-
-			$main['file_limit'] = get_cfg_var ("upload_max_filesize")?get_cfg_var ("upload_max_filesize"):"不允许上传附件"; 
-
-			$this->assign('main',$main);
-			$this->display('main');
-		}
-
 		//清理缓存 
 		private function _deleteDir($R){
 			$handle = opendir($R);
